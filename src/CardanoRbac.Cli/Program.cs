f73028c03f85c7988aa023352c783a5fec56eb0c
chad <x3haloed@gmail.com>
2021-01-23 16:45:00 -0800

    fixed stdin

--- src/CardanoRbac.Cli/Program.cs
+++ src/CardanoRbac.Cli/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.CommandLine;
+using System.CommandLine.Builder;
 using System.CommandLine.Invocation;
+using System.CommandLine.Parsing;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -8,6 +10,8 @@ namespace CardanoRbac.Cli
 {
     class Program
     {
+        public static Stream _inputStream = null;
+
         static async Task<int> Main(string[] args)
         {
             var rootCommand = new RootCommand
@@ -25,21 +29,36 @@ namespace CardanoRbac.Cli
             validateCommand.Description = "Validate an RBAC policy file.";
             validateCommand.Handler = CommandHandler.Create<FileInfo>(async file =>
             {
-                string fileContents = file == null ? "null" : await ReadFileAsync(file);
-                Console.WriteLine($"The value for --file is:\n{fileContents}");
+                if (file != null)
+                {
+                    _inputStream = File.OpenRead(file.FullName);
+                }
+
+                if (_inputStream == null)
+                {
+                    throw new Exception("No input provided.");
+                }
+
+                string isStreamNull = _inputStream == null ? "null" : "not-null";
+                Console.WriteLine($"The value for _inputStrean is: {isStreamNull}");
             });
             rootCommand.Add(validateCommand);
 
-            // Parse the incoming args and invoke the handler
-            return await rootCommand.InvokeAsync(args);
-        }
-
-        private static async Task<string> ReadFileAsync(FileInfo file)
-        {
-            using (StreamReader sr = new StreamReader(File.OpenRead(file.FullName)))
+            var commandLineBuilder = new CommandLineBuilder(rootCommand);
+            commandLineBuilder.UseMiddleware(async (context, next) =>
             {
-                return await sr.ReadToEndAsync();
-            }
+                if (context.Console.IsInputRedirected)
+                {
+                    _inputStream = Console.OpenStandardInput();
+                }
+
+                await next(context);
+            });
+
+            commandLineBuilder.UseDefaults();
+            var parser = commandLineBuilder.Build();
+            // Parse the incoming args and invoke the handler
+            return await parser.InvokeAsync(args);
         }
     }
 }
