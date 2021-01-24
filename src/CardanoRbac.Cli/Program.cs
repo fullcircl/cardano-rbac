2f9201d141c6d25ef0014824104f7006c660eb3d
chad <x3haloed@gmail.com>
2021-01-23 16:31:00 -0800

    Fixed logic without stdin

--- src/CardanoRbac.Cli/Program.cs
+++ src/CardanoRbac.Cli/Program.cs
@@ -1,71 +1,42 @@
 ﻿using System;
 using System.CommandLine;
-using System.CommandLine.Builder;
 using System.CommandLine.Invocation;
-using System.CommandLine.Parsing;
 using System.IO;
-using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace CardanoRbac.Cli
 {
     class Program
     {
-        private static Stream _inputStream = null;
-
         static async Task<int> Main(string[] args)
         {
             var rootCommand = new RootCommand
             {
             };
+            rootCommand.Description = "Cardano RBAC CLI";
 
             var validateCommand = new Command("validate")
             {
                 new Option<FileInfo>(
                     "--file",
                     "An option whose argument is parsed as a FileInfo"
-                ).ExistingOnly()
+                ).ExistingOnly(),
             };
-            validateCommand.Handler =
-                CommandHandler.Create<FileInfo>(async (fileInfo) =>
-                {
-                    if (fileInfo != null)
-                    {
-                        _inputStream = new FileStream(fileInfo.FullName, FileMode.Open, FileAccess.Read);
-                    }
-
-                    if (_inputStream == null)
-                    {
-                        throw new Exception(nameof(fileInfo));
-                    }
-
-                    using (StreamReader sr = new StreamReader(_inputStream))
-                    {
-                        Console.Out.Write(await sr.ReadToEndAsync());
-                    }
-                });
+            validateCommand.Description = "Validate an RBAC policy file.";
+            validateCommand.Handler = CommandHandler.Create<FileInfo>(async file =>
+            {
+                string fileContents = file == null ? "null" : await ReadFileAsync(file);
+                Console.WriteLine($"The value for --file is:\n{fileContents}");
+            });
             rootCommand.Add(validateCommand);
 
-            var commandLineBuilder = new CommandLineBuilder(rootCommand);
-            commandLineBuilder.UseMiddleware(async (context, next) =>
-            {
-                if (context.Console.IsInputRedirected)
-                {
-                    _inputStream = Console.OpenStandardInput();
-                }
-
-                await next(context);
-            });
-
-            commandLineBuilder.UseDefaults();
-            var parser = commandLineBuilder.Build();
-            return await parser.InvokeAsync(args);
+            // Parse the incoming args and invoke the handler
+            return await rootCommand.InvokeAsync(args);
         }
 
-        private static async Task<string> ReadStdInputAsync()
+        private static async Task<string> ReadFileAsync(FileInfo file)
         {
-            using (StreamReader sr = new StreamReader(Console.OpenStandardInput()))
+            using (StreamReader sr = new StreamReader(File.OpenRead(file.FullName)))
             {
                 return await sr.ReadToEndAsync();
             }
